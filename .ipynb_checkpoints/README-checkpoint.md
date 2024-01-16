590e768d14b1298c920d1592155c84cbdc8f08d9
irena-kroc <kroc.irena@gmail.com>
2024-01-15 18:05:52 -0700

    I edited README.md and created new notebook
    I modified slightly the README.md file and I created a new test notebook.

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -1,4 +1,2 @@
 # engo645_repo_project
 This is repository for project
-
-Nice to be here!
---
+++ test.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.0rc0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+('Nice to be here')
+
+# %%
